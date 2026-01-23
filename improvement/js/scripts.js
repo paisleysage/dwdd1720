6cdd273a4e7cc89fc35df941fd8afd4aff794ef7
Paisley Sage <paisley_cooper@icloud.com>
2026-01-23 13:27:54 -0700

    add java for calculator

--- improvement/js/scripts.js
+++ improvement/js/scripts.js
@@ -1,1 +1,17 @@
-console.log("I am connected");
+document.querySelector('#calculate').addEventListener('click', () => {
+const width = Number (document.querySelector('#width').value);
+console.log(width);
+
+const depth = Number (document.querySelector('#depth').value);
+console.log(depth);
+
+const height = Number (document.querySelector('#height').value);
+console.log(height);
+
+const paintType = document.querySelector('#quality').selectedOptions[0].text;
+console.log(paintType)
+
+const quality = document.querySelector('#quality').selectedOptions[0].value;
+console.log(quality)
+
+}) // end of the calculate function
