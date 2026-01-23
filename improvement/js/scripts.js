7f867dd70f7bc5cc3c5417cd7551b0a6befe3c7e
Paisley Sage <paisley_cooper@icloud.com>
2026-01-23 13:49:56 -0700

    get info

--- improvement/js/scripts.js
+++ improvement/js/scripts.js
@@ -14,4 +14,20 @@ console.log(paintType)
 const quality = document.querySelector('#quality').selectedOptions[0].value;
 console.log(quality)
 
+let carpet = Math.ceil((width * depth) /9)
+console.log(carpet + " yards of carpet")
+
+let tackstrip = width + depth + width + depth
+console.log(tackstrip + " feet of tackstrip")
+
+let walls = ((width * height) + (depth * height)) * 2
+walls = Math.ceil(walls/quality)
+console.log(walls + " gallons of semi-gloss paint for the walls")
+
+let ceiling = Math.ceil((width * depth) / quality)
+console.log(ceiling + " gallons of flat paint for the ceiling")
+
+let primer = ceiling + walls
+console.log(primer + " gallons of primer")
+
 }) // end of the calculate function
