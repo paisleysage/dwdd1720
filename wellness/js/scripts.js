d5cea46d106345a73a741ec2ab756f0a8b5d5c1e
Paisley Sage <paisley_cooper@icloud.com>
2026-01-30 12:39:23 -0700

    saving data

--- wellness/js/scripts.js
+++ wellness/js/scripts.js
@@ -1,1 +1,13 @@
-console.log("I am connected");
+const saveBtn = document.querySelector('#saveProfile');
+
+saveBtn.addEventListener('click', () => {
+let fn = document.querySelector('input[name="first"]');
+localStorage.setItem('wellness-first',fn.value);
+let ln = document.querySelector('input[name="last"]');
+localStorage.setItem('wellness-last',ln.value);
+let select = document.querySelector('#physical');
+let ev = select.options [select.selectedIndex].value;
+localStorage.setItem('wellness-emoji', ev)
+let et = select.options [select.selectedIndex].text;
+localStorage.setItem('wellness-text', et)
+})
