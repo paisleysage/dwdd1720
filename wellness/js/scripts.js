6662b8f88f4b1f970f5fa3b2a19e477b81f1e08f
Paisley Sage <paisley_cooper@icloud.com>
2026-01-30 12:58:09 -0700

    conditional loading

--- wellness/js/scripts.js
+++ wellness/js/scripts.js
@@ -1,29 +1,40 @@
-const saveBtn = document.querySelector('#saveProfile');
+const saveBtn = document.querySelector("#saveProfile");
 
-saveBtn.addEventListener('click', () => {
-let fn = document.querySelector('input[name="first"]');
-localStorage.setItem('wellness-first',fn.value);
-let ln = document.querySelector('input[name="last"]');
-localStorage.setItem('wellness-last',ln.value);
-let select = document.querySelector('#physical');
-let ev = select.options [select.selectedIndex].value;
-localStorage.setItem('wellness-emoji', ev)
-let et = select.options [select.selectedIndex].text;
-localStorage.setItem('wellness-text', et)
-})
+saveBtn.addEventListener("click", () => {
+  let fn = document.querySelector('input[name="first"]');
+  localStorage.setItem("wellness-first", fn.value);
+  let ln = document.querySelector('input[name="last"]');
+  localStorage.setItem("wellness-last", ln.value);
+  let select = document.querySelector("#physical");
+  let ev = select.options[select.selectedIndex].value;
+  localStorage.setItem("wellness-emoji", ev);
+  let et = select.options[select.selectedIndex].text;
+  localStorage.setItem("wellness-text", et);
+});
 
+const deleteBtn = document.querySelector("#delProfile");
+deleteBtn.addEventListener("click", () => {
+  localStorage.removeItem("wellness-first");
+  localStorage.removeItem("wellness-last");
+  localStorage.removeItem("wellness-text");
+  localStorage.removeItem("wellness-emoji");
+  location.reload();
+});
 
-const deleteBtn = document.querySelector('#delProfile')
-deleteBtn.addEventListener('click' , () => {
-localStorage.removeItem('wellness-first')
-localStorage.removeItem('wellness-last')
-localStorage.removeItem('wellness-text')
-localStorage.removeItem('wellness-emoji')
-location.reload();
-})
-
-
-document.querySelector('#first').textContent = localStorage.getItem('wellness-first')
-document.querySelector('#last').textContent = localStorage.getItem('wellness-last')
-document.querySelector('#emotion').textContent = localStorage.getItem('wellness-text')
-document.querySelector('#emoji').src = localStorage.getItem('wellness-emoji')
+const check = localStorage.getItem("wellness-first");
+console.log(check);
+if (check == null) {
+  document.querySelector("#newProfile").className = "showMe";
+  document.querySelector("#myProfile").className = "hideMe";
+  // do nothing
+} else {
+  document.querySelector("#first").textContent =
+    localStorage.getItem("wellness-first");
+  document.querySelector("#last").textContent =
+    localStorage.getItem("wellness-last");
+  document.querySelector("#emotion").textContent =
+    localStorage.getItem("wellness-text");
+  document.querySelector("#emoji").src = localStorage.getItem("wellness-emoji");
+  document.querySelector("#newProfile").className = "hideMe";
+  document.querySelector("#myProfile").className = "showMe";
+}
