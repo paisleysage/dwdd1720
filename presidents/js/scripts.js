75dd69dba569c549eba434da0eeb1acf101b3325
Paisley Sage <paisley_cooper@icloud.com>
2026-02-07 10:40:27 -0700

    styling

--- presidents/js/scripts.js
+++ presidents/js/scripts.js
@@ -1,30 +1,31 @@
-import {presidents} from '../data/presidents.js'
-console.log(presidents)
+import { presidents } from "../data/presidents.js";
+console.log(presidents);
 
-const pathStart = "//resources.dgmuvu.com/presidents/"
+const pathStart = "//resources.dgmuvu.com/presidents/";
 
-const myTarget = document.querySelector('#cards')
+const myTarget = document.querySelector("#cards");
 
 for (let x = 0; x < presidents.length; x++) {
-let barDiv = document.createElement('div')
-barDiv.innerHTML = `<p>${presidents[x].name}</p>`
+  let barDiv = document.createElement("div");
 
-let myImage = document.createElement('img')
-myImage.setAttribute("src", pathStart+presidents[x].photo)
-myImage.setAttribute("alt", presidents[x].name)
+  barDiv.classList.add('bar');
 
+  barDiv.innerHTML = `<p>${presidents[x].name}</p>`;
 
-let myCaption = document.createElement('figcaption')
-myCaption.innerHTML = `Served ${presidents[x].took_office} to ${presidents[x].left_office} <br>Lived from ${presidents[x].born} to ${presidents[x].died} <br>Party: ${presidents[x].party}`
+  let myImage = document.createElement("img");
+  myImage.setAttribute("src", pathStart + presidents[x].photo);
+  myImage.setAttribute("alt", presidents[x].name);
 
+  let myCaption = document.createElement("figcaption");
+  myCaption.innerHTML = `Served ${presidents[x].took_office} to ${presidents[x].left_office} <br>Lived from ${presidents[x].born} to ${presidents[x].died} <br>Party: ${presidents[x].party}`;
 
-let myFigure = document.createElement('figure')
+  myCaption.classList.add('caption');
 
-myFigure.appendChild(barDiv)
-myFigure.appendChild(myImage)
-myFigure.appendChild(myCaption)
+  let myFigure = document.createElement("figure");
 
-myTarget.appendChild(myFigure)
+  myFigure.appendChild(barDiv);
+  myFigure.appendChild(myImage);
+  myFigure.appendChild(myCaption);
 
-
+  myTarget.appendChild(myFigure);
 } // end of loop
