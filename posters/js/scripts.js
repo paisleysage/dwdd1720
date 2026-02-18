22f56e3fe9cdb1ecfcb2ca0f356e470463c08c3e
Paisley Sage <paisley_cooper@icloud.com>
2026-02-18 14:50:33 -0700

    everything pretty much

--- posters/js/scripts.js
+++ posters/js/scripts.js
@@ -1,7 +1,36 @@
+const btnList = document.querySelector("#list");
+const btnGrid = document.querySelector("#grid");
+const pageBody = document.querySelector("body");
 
-const btnRed = document.querySelector("#redImage")
-const btnGreen = document.querySelector("#greenImage")
-const pageBody = document.querySelector("div")
+btnList.addEventListener("click", () => (pageBody.className = "list"));
+btnGrid.addEventListener("click", () => (pageBody.className = "grid"));
 
-btnRed.addEventListener('click' , () => pageBody.className = "paintRed")
-btnGreen.addEventListener('click' , () => pageBody.className = "paintGreen")
+import { films } from "../data/films.js";
+
+const myTarget = document.querySelector("#cards");
+
+for (let x = 0; x < films.length; x++) {
+  console.log(`film: ${films[x]}`);
+  let barDiv = document.createElement("div");
+
+  barDiv.classList.add("bar");
+
+  barDiv.innerHTML = `<h2>${films[x].title}</h2>`;
+
+  let myImage = document.createElement("img");
+  myImage.setAttribute("src", `./images/${films[x].episode_id}.jpeg`);
+  myImage.setAttribute("alt", films[x].title);
+
+  let myCaption = document.createElement("figcaption");
+  myCaption.innerHTML = `Director: ${films[x].director} <br>Producer: ${films[x].producer} <br>Release Date: ${films[x].release_date}`;
+
+  myCaption.classList.add("caption");
+
+  let myFigure = document.createElement("figure");
+
+  myFigure.appendChild(barDiv);
+  myFigure.appendChild(myImage);
+  myFigure.appendChild(myCaption);
+
+  myTarget.appendChild(myFigure);
+} // end of loop
