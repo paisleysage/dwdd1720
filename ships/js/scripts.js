32ee37f9dae139f50ff4071f5bb14155f1673b34
Paisley Sage <paisley_cooper@icloud.com>
2026-03-20 10:30:01 -0600

    added nav

--- ships/js/scripts.js
+++ ships/js/scripts.js
@@ -1,1 +1,27 @@
-console.log("I am connected");
+// add links to the html
+const myNavigation = document.querySelector("nav");
+const myViewer = document.querySelector("main");
+
+// go grab the data and then WAIT for the result
+fetch("../data/starships.json")
+  .then((response) => response.json())
+  .then((shipArray) => {
+    console.log(shipArray);
+    populateNav(shipArray);
+  });
+
+// populate the nav bar
+function populateNav(allShips) {
+  console.log(allShips);
+  allShips.forEach((ship) => {
+    let myButton = document.createElement("button");
+    myButton.textContent = ship.name;
+    myButton.addEventListener("click", () => showShip(ship));
+    myNavigation.appendChild(myButton);
+  }); // end of the loop
+} // end of nav populate
+
+// ship viewer
+function showShip(x) {
+  console.log(x.model);
+}
