d995a4cd1506b23795c632d8b6adfbd03137ce17
Paisley Sage <paisley_cooper@icloud.com>
2026-03-27 12:47:05 -0600

    grab data

--- weather/js/scripts.js
+++ weather/js/scripts.js
@@ -1,1 +1,19 @@
-console.log("I am connected");
+// write a default zipcode
+let zip = "84120";
+
+// set path to api on weather
+const myKey = `14d39f65902a70efe1d8e665dbd48f84`;
+const myPath = `https://api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${myKey}&units=imperial`;
+
+// fetch remote json data for the current weather
+fetch(myPath)
+  .then((response) => response.json())
+  .then((allData) => {
+    console.log(allData);
+  });
+
+// function thta displays the current weather
+
+// ask for a new zipcode
+
+// data validation for the zipcode
